375d74491d8027e7834831e13050a8b6c9382fdb
Malik Ghulam Subbhani <thewebster0326@gmail.com>
2026-08-17 09:03:43 +0200

    Scaffold Eleventy project, migrate assets, add global data files

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/php": "." });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return new Date(dateObj).toLocaleDateString("en-ZA", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByTag("post").sort((a, b) => b.date - a.date);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
+    },
+  };
+};
